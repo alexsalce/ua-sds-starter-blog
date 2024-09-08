b866ed3cf85c07557694af0c8e753927722f66e6
alexsalce <101913154+alexsalce@users.noreply.github.com>
2024-09-08 04:20:08 +0000

    default config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,8 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.pink,
-        gray: colors.gray,
+        primary: colors.rose,
+        gray: colors.slate,
       },
       zIndex: {
         60: '60',
