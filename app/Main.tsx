9872eed110c4e520dc6cea7817cca4d539666822
alexsalce <101913154+alexsalce@users.noreply.github.com>
2024-09-08 20:47:22 +0000

    added hero components, main updates | added emotion, sharp packages

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,12 +3,17 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { components } from '@/components/MDXComponents'
+
+// import custom SDSHero (sds_custom)
+import SDSHero from '@/components/SDSHero'
 
 const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
   return (
     <>
+      <SDSHero />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
