2048d71f64d9c160194e19fd12b8f487e03ccad0
alexsalce <101913154+alexsalce@users.noreply.github.com>
2024-10-11 19:44:13 +0000

    site dev post updates

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -23,7 +23,7 @@ const siteMetadata = {
   instagram: 'https://www.instagram.com',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
-  stickyNav: false,
+  stickyNav: true,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
