3114ef5ac09ad0e38cba043500bfbe450323f4ba
alexsalce <101913154+alexsalce@users.noreply.github.com>
2024-12-16 11:39:30 +0000

    Cirriculum Vitae incorrect spelling, Curriculum Vitae

--- layouts/ResumeLayout.tsx
+++ layouts/ResumeLayout.tsx
@@ -22,7 +22,7 @@ function ResumeLayout({ children, toc }: LayoutProps) {
         <div className="resume opacity-90">
           <header className="space-y-2 pb-8 pt-6 md:space-y-5">
             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Cirriculum Vitae
+              Curriculum Vitae
             </h1>
             <p className="text-base text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
               An overview of my education, career, skills, and publications.
